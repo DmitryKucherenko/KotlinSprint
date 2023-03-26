6c62841863c33856fd8af1567e6c4280d6010ac2
Dmitry K <fatalzero@Mail.ru>
2023-03-26 20:44:54 +0300

    KS-1-4 declaration of sense variables and output on console

---
+++ src/main/kotlin/lesson_1/lesson_1_task_4.kt
@@ -0,0 +1,17 @@
+package lesson_1
+
+fun main(){
+    val length:Long = 40_868_600_000
+    val age:UByte = 27U
+    val partOfDay:Float = 0.075f
+    val seconds:UShort = 6_480U
+    val partOfYear:Double = 2.0547945205479453E-4
+    val apogee:Int = 327_000
+
+    println("Length: $length")
+    println("Age: $age")
+    println("Part of day: $partOfDay")
+    println("Seconds: $seconds")
+    println("Part of year: $partOfYear")
+    println("Apogee: $apogee")
+}
