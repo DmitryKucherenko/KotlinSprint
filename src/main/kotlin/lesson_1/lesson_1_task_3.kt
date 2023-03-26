9df7457fa6b4c39fa1228103c7ac050c483a925e
Dmitry K <fatalzero@Mail.ru>
2023-03-26 20:21:16 +0300

    @DmitryKucherenko KS-1-3 declaration of sense variables and output on console

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,16 @@
+package lesson_1
+
+fun main() {
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println("Year of launch: $year")
+    println("hour of launch: $hour")
+    println("minute of launch: $minute")
+
+    hour = 10
+    minute = 55
+
+    println("Boarding time: $hour : $minute")
+}
