3e9741e076b5ecd11c51e73afe0ced9da8c0635c
Dmitry K <fatalzero@Mail.ru>
2023-03-26 20:02:58 +0300

    @DmitryKucherenko KS-1-2 add declaration of sense variable employeesCount

---
+++ src/main/kotlin/lesson_1/lesson_2_task_2.kt
@@ -0,0 +1,11 @@
+package lesson_1
+
+fun main() {
+    var orderCount: Int = 75
+    var thankMessage: String = "Thank you very mach!"
+    println("Number of orders: $orderCount, thank: $thankMessage")
+    var employeesCount: Int = 2000
+    //println("Number of employee: $employeesCount")
+    employeesCount--
+    println("Number of employee: $employeesCount")
+}
