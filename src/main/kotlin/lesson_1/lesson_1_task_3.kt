6b1f6b78ca9bc77318dee91f56129114520292f4
Dmitry K <fatalzero@Mail.ru>
2023-03-27 19:58:34 +0300

    KS-1-5 fix time output

--- src/main/kotlin/lesson_1/lesson_1_task_3.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -6,11 +6,14 @@ fun main() {
     var minute: Int = 7
 
     println("Year of launch: $year")
-    println("hour of launch: $hour")
-    println("minute of launch: $minute")
+    println("hour of launch: ${hour.toTimeString()}")
+    println("minute of launch: ${minute.toTimeString()}")
 
     hour = 10
     minute = 55
 
-    println("Boarding time: $hour : $minute")
+    println("Boarding time:  ${hour.toTimeString()} : ${minute.toTimeString()}")
+    println(5.toTimeString())
 }
+
+fun Int.toTimeString() = String.format("%02d", this)
