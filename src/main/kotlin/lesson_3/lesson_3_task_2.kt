3a4429844d5691d4f494a06c56ca6c0844cd48a9
Dmitry K <fatalzero@Mail.ru>
2023-03-28 21:22:15 +0300

    KS-3-2 declaration of sense variables and output FIO, age

---
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -0,0 +1,12 @@
+package lesson_3
+
+fun main() {
+    var firstName = "Татьяна"
+    var lastName = "Андреева"
+    val surName = "Сергеевна"
+    var age = 20
+    println("$lastName $firstName $surName, $age")
+    lastName = "Сидорова"
+    age = 22
+    println("$lastName $firstName $surName, $age")
+}
