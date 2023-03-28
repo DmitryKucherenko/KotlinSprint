7b2e4fc80750cc1faf8eac86a3d74467f5e7b8a3
Dmitry K <fatalzero@Mail.ru>
2023-03-28 21:40:25 +0300

    KS-3-3 declaration of sense variables and output multiply table

---
+++ src/main/kotlin/lesson_3/lesson_3_task_1.kt
@@ -0,0 +1,21 @@
+package lesson_3
+
+fun main() {
+
+    var multiplyNumber = 6
+
+    println(
+        """
+        $multiplyNumber x 1 = ${multiplyNumber * 1} 
+        $multiplyNumber x 2 = ${multiplyNumber * 2} 
+        $multiplyNumber x 3 = ${multiplyNumber * 3} 
+        $multiplyNumber x 4 = ${multiplyNumber * 4} 
+        $multiplyNumber x 5 = ${multiplyNumber * 5} 
+        $multiplyNumber x 6 = ${multiplyNumber * 6} 
+        $multiplyNumber x 7 = ${multiplyNumber * 7} 
+        $multiplyNumber x 8 = ${multiplyNumber * 8} 
+        $multiplyNumber x 9 = ${multiplyNumber * 9} 
+        """.trimIndent()
+    )
+
+}
