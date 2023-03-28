fde6830b38b56384f62f52d62df38bc423531ea5
Dmitry K <fatalzero@Mail.ru>
2023-03-28 22:15:24 +0300

    KS-2-4 Add space CTR + ALT + L :)

--- src/main/kotlin/lesson_2/lesson2_task_4.kt
+++ src/main/kotlin/lesson_2/lesson2_task_4.kt
@@ -5,7 +5,7 @@ fun main() {
     val iron = 11
     val buff = 0.2
 
-    println(String.format("Crystal:%.0f",crystal*buff))
-    println(String.format("Iron:%.0f",iron*buff))
+    println(String.format("Crystal:%.0f", crystal * buff))
+    println(String.format("Iron:%.0f", iron * buff))
 
 }
