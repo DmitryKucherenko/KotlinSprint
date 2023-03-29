130015421fb92b1fc385b84c90899e53b69f52d4
Dmitry K <fatalzero@Mail.ru>
2023-03-29 23:09:03 +0300

    KS-4-4 program for exercises

---
+++ src/main/kotlin/lesson_4/lesson_4_task4.kt
@@ -0,0 +1,19 @@
+package lesson_4
+
+fun main() {
+    var dayOfTrain = 5
+    var exerciseForArms = dayOfTrain % 2 != 0
+    var exerciseForLags = dayOfTrain % 2 == 0
+    var exerciseForBack = dayOfTrain % 2 == 0
+    var exerciseForPress = dayOfTrain % 2 != 0
+
+    println(
+        """
+           Упражнения для рук:    ${exerciseForArms}
+           Упражнения для ног:    ${exerciseForLags}
+           Упражнения для спины:  ${exerciseForBack}
+           Упражнения для пресса: ${exerciseForPress}
+        """.trimIndent()
+    )
+
+}
