d281ddd1072dd0a3d26faf8f7ad63540682ccd61
Dmitry K <fatalzero@Mail.ru>
2023-03-30 20:36:07 +0300

    KS 4-3 fix variable's name

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -1,11 +1,14 @@
 package lesson_4
 
 fun main() {
-    val sunnyWeather = false
+    val sunnyWeather = true
     val isAwningOpen = true
     val humidity = 20
-    val isWinter = false
+    val isNotWinter = false
 
-    println("Благоприятные ли условия сейчас для роста бобовых?${sunnyWeather == true && isAwningOpen == true && humidity == 20 && isWinter == false}")
+    println(
+        "Благоприятные ли условия сейчас для роста бобовых?" +
+                " ${sunnyWeather == true && isAwningOpen == true && humidity == 20 && isNotWinter == true}"
+    )
 
 }
