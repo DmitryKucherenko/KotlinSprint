faad6dd1ce7c0a7e59a641e61abb51f945102781
Dmitry K <fatalzero@Mail.ru>
2023-03-30 20:30:31 +0300

    KS 4-2 fix magic numbers :)

--- src/main/kotlin/lesson_4/lesson_4_task_2.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_2.kt
@@ -1,8 +1,13 @@
 package lesson_4
 
 fun main(){
-    println("Average для груза с весом 20 кг и объемом 80 л: ${20 >= MIN_AVERAGE_WEIGHT && 20 <= MAX_AVERAGE_WEIGHT && 80 < MAX_AVERAGE_VOLUME}")
-    println("Average для груза с весом 50 кг и объемом 100 л: ${50 >= MIN_AVERAGE_WEIGHT && 50 <= MAX_AVERAGE_WEIGHT && 100 < MAX_AVERAGE_VOLUME}")
+    val cargoWeight1 = 20
+    val cargoVolume1 = 80
+    val cargoWeight2 = 50
+    val cargoVolume2 = 100
+
+    println("Average для груза с весом $cargoWeight1 кг и объемом $cargoVolume1 л: ${cargoWeight1 >= MIN_AVERAGE_WEIGHT && cargoWeight1 <= MAX_AVERAGE_WEIGHT && cargoVolume1 < MAX_AVERAGE_VOLUME}")
+    println("Average для груза с весом $cargoWeight2 кг и объемом $cargoVolume2 л: ${cargoWeight2 >= MIN_AVERAGE_WEIGHT && cargoWeight2 <= MAX_AVERAGE_WEIGHT && cargoVolume2 < MAX_AVERAGE_VOLUME}")
 }
 
 const val MIN_AVERAGE_WEIGHT = 35
